31d52be2ae581e1cefa07313f492935bf0efc7e8
LLinkIsMyName <144070593+LLinkIsMyName@users.noreply.github.com>
2023-12-27 19:24:00 +0200

    upt

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,17 @@
 class Storage {
+  #items;
   constructor(items) {
     this.#items = items;
   }
-
   getItems() {
     return this.#items;
   }
-
   addItem(newItem) {
     this.#items.push(newItem);
   }
-
   removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
+    const idxToRemove = this.#items.indexOf(itemToRemove);
+    this.#items.splice(idxToRemove, 1);
   }
 }
 
